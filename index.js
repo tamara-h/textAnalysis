46c755a68d21331eed5a22b4e6556d686942fe2a
tamara-h <mixedsoups@gmail.com>
2017-12-15 16:56:00 +0000

    trying to figure out the best way to split sentences

--- index.js
+++ index.js
@@ -12,21 +12,64 @@ let doc = nlp(text);
 
 // console.log(doc.out('tags'));
 
+//
+// fs.readFile('randomText.txt', 'utf8', function(err, contents) {
+//
+//     let array = nlp(contents).nouns().out('freq');
+//     let newArray = [];
+//
+//     for(i=0;i<array.length;i++){
+//         // console.log(array[i]);
+//         if(array[i].count > 3){
+//             newArray.push(array[i]);
+//         }
+//     }
+//
+//     console.log(newArray);
+// });
 
-fs.readFile('randomText.txt', 'utf8', function(err, contents) {
 
-    let array = nlp(contents).nouns().out('freq');
-    let newArray = [];
+//split text up regexp by full stops
+//for each sentence work out the structure
+//seperate array for each type, maybe with a count?
+//get most popular sentence structure, fill it with random words of each type.
 
-    for(i=0;i<array.length;i++){
-        // console.log(array[i]);
-        if(array[i].count > 3){
-            newArray.push(array[i]);
-        }
+
+fs.readFile('text.txt', 'utf8', function(err, contents) {
+
+    let sentences = nlp(contents).sentences().out('array');
+
+
+    for(i=0;i<sentences.length;i++){
+        // let arrayOfStuff = nlp(sentences[i]).terms(0).data()[0].tags;
+        //for each word in the sentence
+        // let words = sentences[i].split(" ");
+        // console.log(words);
+        console.log(nlp(sentences[i]).out("array"));
+
+
+        // console.log(words.length);
+        // console.log(words[0].bestTag);
+        // for(j=0; words.length;j++){
+        //     //get the relevent tag
+        //
+        //     // console.log('here');
+        //     console.log(words[j]);
+        // }
+
+
+
+
+        // console.log(arrayOfStuff)
     }
 
-    console.log(newArray);
+
+
+
+
+
 });
+
 
 
 
